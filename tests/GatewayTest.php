7087c87285e3329e04516737986f532ad46aab3e
Chris Abernethy <chris.abernethy@zerve.com>
2015-12-03 11:58:24 -0500

    Add ReferenceNumber to returns

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -24,7 +24,8 @@ class GatewayTest extends GatewayTestCase
 
         // Configure enough to pass validation.
         $this->refundOptions = array(
-            'amount' => '10.00'
+            'amount'        => '10.00',
+            'transactionId' => '566073022feca'
         );
     }
 
@@ -55,6 +56,10 @@ class GatewayTest extends GatewayTestCase
         $this->setMockHttpResponse('RefundSuccess.txt');
         $response = $this->gateway->refund($this->refundOptions)->send();
         $this->assertTrue($response->isSuccessful());
+        $this->assertSame('2005846634', $response->getTransactionReference());
+        $this->assertSame($this->refundOptions['transactionId'], $response->getTransactionId());
+        $this->assertSame('Approved', $response->getMessage());
+        $this->assertSame('0', $response->getCode());
     }
 
     public function testRefundFailure()
@@ -62,5 +67,9 @@ class GatewayTest extends GatewayTestCase
         $this->setMockHttpResponse('RefundFailure.txt');
         $response = $this->gateway->refund($this->refundOptions)->send();
         $this->assertFalse($response->isSuccessful());
+        $this->assertSame('2005846625', $response->getTransactionReference());
+        $this->assertSame($this->refundOptions['transactionId'], $response->getTransactionId());
+        $this->assertSame('Declined', $response->getMessage());
+        $this->assertSame('20', $response->getCode());
     }
 }
