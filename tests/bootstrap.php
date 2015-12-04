000129892f89338be43c6e71aeaa459bf1482c75
Chris Abernethy <chris.abernethy@zerve.com>
2015-12-04 11:46:16 -0500

    Add support for PHP_Token_ELLIPSIS to php tokenizer

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+// include the composer autoloader
+$autoloader = require __DIR__.'/../vendor/autoload.php';
+
+// Back-port https://github.com/sebastianbergmann/php-token-stream/commit/3f60c2ac940e9d0dc46cc8be369ec95d72367e06
+class PHP_Token_ELLIPSIS extends PHP_Token {}
