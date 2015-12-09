1cf932088b51813012be40e99da3804dc1ebc281
Chris Abernethy <chris.abernethy@zerve.com>
2015-12-09 16:22:38 -0500

    Add tests for PaymentAccount model

--- src/Model/PaymentAccount.php
+++ src/Model/PaymentAccount.php
@@ -1,6 +1,7 @@
 <?php
 namespace Omnipay\ElementExpress\Model;
 
+use Omnipay\Common\Exception\InvalidRequestException;
 use Omnipay\ElementExpress\Enumeration\PaymentAccountType;
 
 class PaymentAccount extends AbstractModel
@@ -29,5 +30,30 @@ class PaymentAccount extends AbstractModel
             $node->appendChild(new \DOMElement('PaymentAccountType', $this['PaymentAccountType']->value()));
         }
         $node->appendChild(new \DOMElement('PaymentAccountReferenceNumber', $this['PaymentAccountReferenceNumber']));
+    }
+
+
+    /**
+     * Model validation ensures that any data that is present in the model is
+     * formatted correctly. No business logic validation is performed at this
+     * level.
+     *
+     * @throws InvalidRequestException if validation fails.
+     */
+    public function validate()
+    {
+        if (strlen($this['cardReference']) && !preg_match('/^.{1,50}$/', $this['cardReference'])) {
+            throw new InvalidRequestException('cardReference should 50 or fewer characters');
+        }
+
+        if (isset($this['PaymentAccountType']) && !$this['PaymentAccountType'] instanceof PaymentAccountType) {
+            throw new InvalidRequestException('Invalid value for PaymentAccountType');
+        }
+
+        if (strlen($this['PaymentAccountReferenceNumber'])) {
+            if (!preg_match('/^.{1,50}$/', $this['PaymentAccountReferenceNumber'])) {
+                throw new InvalidRequestException('PaymentAccountReferenceNumber should 50 or fewer characters');
+            }
+        }
     }
 }
---
+++ tests/Model/PaymentAccountTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace Omnipay\ElementExpress\Tests\Model;
+
+use Omnipay\ElementExpress\Model\PaymentAccount;
+use Omnipay\ElementExpress\Enumeration\PaymentAccountType;
+use Omnipay\Tests\TestCase;
+
+class PaymentAccountTest extends TestCase
+{
+    //
+    // Generic Data Validation
+    //
+
+    public function invalidData()
+    {
+        return [
+            'cardReference: too-long'                 => ['cardReference', str_repeat('x', 51)],
+            'PaymentAccountType: invalid-value'       => ['PaymentAccountType', 0],
+            'PaymentAccountReferenceNumber: too-long' => ['PaymentAccountReferenceNumber', str_repeat('x', 51)],
+        ];
+    }
+
+    public function validData()
+    {
+        return [
+            'cardReference: max-length'                 => ['cardReference', str_repeat('x', 50)],
+            'PaymentAccountType: max-length'            => ['PaymentAccountType', PaymentAccountType::CREDIT_CARD()],
+            'PaymentAccountReferenceNumber: max-length' => ['PaymentAccountReferenceNumber', str_repeat('x', 50)],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidData
+     * @expectedException \Omnipay\Common\Exception\InvalidRequestException
+     */
+    public function testDataValidationFails($field, $value)
+    {
+        $model = new PaymentAccount();
+        $model[$field] = $value;
+        $model->validate();
+    }
+
+    /**
+     * @dataProvider validData
+     */
+    public function testDataValidationSucceeds($field, $value)
+    {
+        $model = new PaymentAccount();
+        $model[$field] = $value;
+        $model->validate();
+    }
+}
