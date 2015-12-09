88c521c82ba95e57c9d6425e7698a95902da0ecd
Chris Abernethy <chris.abernethy@zerve.com>
2015-12-09 16:01:31 -0500

    Add validation for Address model

--- src/Model/Address.php
+++ src/Model/Address.php
@@ -1,6 +1,8 @@
 <?php
 namespace Omnipay\ElementExpress\Model;
 
+use Omnipay\Common\Exception\InvalidRequestException;
+
 class Address extends AbstractModel
 {
     public function getDefaultParameters()
@@ -52,5 +54,90 @@ class Address extends AbstractModel
         $node->appendChild(new \DOMElement('ShippingCity', $this['shippingCity']));
         $node->appendChild(new \DOMElement('ShippingState', $this['shippingState']));
         $node->appendChild(new \DOMElement('ShippingZipcode', $this['shippingPostcode']));
+    }
+
+    /**
+     * Model validation ensures that any data that is present in the model is
+     * formatted correctly. No business logic validation is performed at this
+     * level.
+     *
+     * @throws InvalidRequestException if validation fails.
+     */
+    public function validate()
+    {
+        if (strlen($this['billingName']) && !preg_match('/^.{1,100}$/', $this['billingName'])) {
+            throw new InvalidRequestException('billingName should have 100 or fewer digits');
+        }
+
+        if (strlen($this['billingPhone']) && !preg_match('/^.{1,20}$/', $this['billingPhone'])) {
+            throw new InvalidRequestException('billingPhone should have 20 or fewer digits');
+        }
+
+        if (strlen($this['billingEmail'])) {
+            if (!preg_match('/^.{1,80}$/', $this['billingEmail'])) {
+                throw new InvalidRequestException('billingEmail should have 80 or fewer digits');
+            }
+            if (!filter_var($this['billingEmail'], FILTER_VALIDATE_EMAIL)) {
+                throw new InvalidRequestException('billingEmail is invalid');
+            }
+        }
+
+        if (strlen($this['billingAddress1']) && !preg_match('/^.{1,50}$/', $this['billingAddress1'])) {
+            throw new InvalidRequestException('billingAddress1 should have 50 or fewer digits');
+        }
+
+        if (strlen($this['billingAddress2']) && !preg_match('/^.{1,50}$/', $this['billingAddress2'])) {
+            throw new InvalidRequestException('billingAddress2 should have 50 or fewer digits');
+        }
+
+        if (strlen($this['billingCity']) && !preg_match('/^.{1,40}$/', $this['billingCity'])) {
+            throw new InvalidRequestException('billingCity should have 40 or fewer digits');
+        }
+
+        if (strlen($this['billingState']) && !preg_match('/^.{1,30}$/', $this['billingState'])) {
+            throw new InvalidRequestException('billingState should have 30 or fewer digits');
+        }
+
+        if (strlen($this['billingPostcode']) && !preg_match('/^.{1,20}$/', $this['billingPostcode'])) {
+            throw new InvalidRequestException('billingPostcode should have 20 or fewer digits');
+        }
+
+        if (strlen($this['shippingName']) && !preg_match('/^.{1,100}$/', $this['shippingName'])) {
+            throw new InvalidRequestException('shippingName should have 100 or fewer digits');
+        }
+
+        if (strlen($this['shippingPhone']) && !preg_match('/^.{1,20}$/', $this['shippingPhone'])) {
+            throw new InvalidRequestException('shippingPhone should have 20 or fewer digits');
+        }
+
+        if (strlen($this['shippingEmail'])) {
+            if (!preg_match('/^.{1,80}$/', $this['shippingEmail'])) {
+                throw new InvalidRequestException('shippingEmail should have 80 or fewer digits');
+            }
+            if (!filter_var($this['shippingEmail'], FILTER_VALIDATE_EMAIL)) {
+                throw new InvalidRequestException('shippingEmail is invalid');
+            }
+        }
+
+        if (strlen($this['shippingAddress1']) && !preg_match('/^.{1,50}$/', $this['shippingAddress1'])) {
+            throw new InvalidRequestException('shippingAddress1 should have 50 or fewer digits');
+        }
+
+        if (strlen($this['shippingAddress2']) && !preg_match('/^.{1,50}$/', $this['shippingAddress2'])) {
+            throw new InvalidRequestException('shippingAddress2 should have 50 or fewer digits');
+        }
+
+        if (strlen($this['shippingCity']) && !preg_match('/^.{1,40}$/', $this['shippingCity'])) {
+            throw new InvalidRequestException('shippingCity should have 40 or fewer digits');
+        }
+
+        if (strlen($this['shippingState']) && !preg_match('/^.{1,30}$/', $this['shippingState'])) {
+            throw new InvalidRequestException('shippingState should have 30 or fewer digits');
+        }
+
+        if (strlen($this['shippingPostcode']) && !preg_match('/^.{1,20}$/', $this['shippingPostcode'])) {
+            throw new InvalidRequestException('shippingPostcode should have 20 or fewer digits');
+        }
+
     }
 }
---
+++ tests/Model/AddressTest.php
@@ -0,0 +1,79 @@
+<?php
+namespace Omnipay\ElementExpress\Tests\Model;
+
+use Omnipay\ElementExpress\Model\Address;
+use Omnipay\Tests\TestCase;
+
+class AddressTest extends TestCase
+{
+    //
+    // Generic Data Validation
+    //
+
+    public function invalidData()
+    {
+        return [
+            'billingName: too-long'          => ['billingName', str_repeat('x', 101)],
+            'billingPhone: too-long'         => ['billingPhone', str_repeat('x', 21)],
+            'billingEmail: too-long'         => ['billingEmail', sprintf("%'x37s@%'y39s.com", 'devops', 'zerve')],
+            'billingEmail: bad-format'       => ['billingEmail', 'devopszerve.com'],
+            'billingAddress1: too-long'      => ['billingAddress1', str_repeat('x', 51)],
+            'billingAddress2: too-long'      => ['billingAddress2', str_repeat('x', 51)],
+            'billingCity: too-long'          => ['billingCity', str_repeat('x', 41)],
+            'billingState: too-long'         => ['billingState', str_repeat('x', 31)],
+            'billingPostcode: too-long'      => ['billingPostcode', str_repeat('x', 21)],
+            'shippingName: too-long'         => ['shippingName', str_repeat('x', 101)],
+            'shippingEmail: too-long'        => ['shippingEmail', sprintf("%'x37s@%'y39s.com", 'devops', 'zerve')],
+            'shippingEmail: bad-format'      => ['shippingEmail', 'devopszerve.com'],
+            'shippingPhone: too-long'        => ['shippingPhone', str_repeat('x', 21)],
+            'shippingAddress1: too-long'     => ['shippingAddress1', str_repeat('x', 51)],
+            'shippingAddress2: too-long'     => ['shippingAddress2', str_repeat('x', 51)],
+            'shippingCity: too-long'         => ['shippingCity', str_repeat('x', 41)],
+            'shippingState: too-long'        => ['shippingState', str_repeat('x', 31)],
+            'shippingPostcode: too-long'     => ['shippingPostcode', str_repeat('x', 21)],
+        ];
+    }
+
+    public function validData()
+    {
+        return [
+            'billingName: max-length'          => ['billingName', str_repeat('x', 100)],
+            'billingEmail: max-length'         => ['billingEmail', sprintf("%'x37s@%'y38s.com", 'devops', 'zerve')],
+            'billingPhone: max-length'         => ['billingPhone', str_repeat('x', 20)],
+            'billingAddress1: max-length'      => ['billingAddress1', str_repeat('x', 50)],
+            'billingAddress2: max-length'      => ['billingAddress2', str_repeat('x', 50)],
+            'billingCity: max-length'          => ['billingCity', str_repeat('x', 40)],
+            'billingState: max-length'         => ['billingState', str_repeat('x', 30)],
+            'billingPostcode: max-length'      => ['billingPostcode', str_repeat('x', 20)],
+            'shippingName: max-length'         => ['shippingName', str_repeat('x', 100)],
+            'shippingEmail: max-length'        => ['shippingEmail', sprintf("%'x37s@%'y38s.com", 'devops', 'zerve')],
+            'shippingPhone: max-length'        => ['shippingPhone', str_repeat('x', 20)],
+            'shippingAddress1: max-length'     => ['shippingAddress1', str_repeat('x', 50)],
+            'shippingAddress2: max-length'     => ['shippingAddress2', str_repeat('x', 50)],
+            'shippingCity: max-length'         => ['shippingCity', str_repeat('x', 40)],
+            'shippingState: max-length'        => ['shippingState', str_repeat('x', 30)],
+            'shippingPostcode: max-length'     => ['shippingPostcode', str_repeat('x', 20)],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidData
+     * @expectedException \Omnipay\Common\Exception\InvalidRequestException
+     */
+    public function testDataValidationFails($field, $value)
+    {
+        $model = new Address();
+        $model[$field] = $value;
+        $model->validate();
+    }
+
+    /**
+     * @dataProvider validData
+     */
+    public function testDataValidationSucceeds($field, $value)
+    {
+        $model = new Address();
+        $model[$field] = $value;
+        $model->validate();
+    }
+}
